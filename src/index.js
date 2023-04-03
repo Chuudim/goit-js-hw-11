f277c655f4afa3b8dc7a90d3a3e03f8a78f0563a
Chuudim <chulanovdima2507@gmail.com>
2023-04-03 13:16:23 +0300

    ДЗ22

--- src/index.js
+++ src/index.js
@@ -1,115 +1,114 @@
+import './sass/index.scss';
+import NewsApiService from './js/api-service';
+import { lightbox } from './js/lightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import Notiflix from "notiflix";
-
-
-window.onload = function () {
-const searchForm = document.getElementById('search-form');
-const searchInput = document.querySelector('input[name="searchQuery"]');
-const searchButton = document.querySelector('button[type="submit"]');
-const apiKey = '34895414-9ef57564a22c698a0997c3c6f';
- searchInput
-
-searchForm.addEventListener('submit', async (event) => {
-  event.preventDefault(); 
-  const searchTerm = searchInput.value;
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    renderImages(data.hits);
-  } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-  }
-});
-
-function renderImages(images) {
-  const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = '';
-
-  if (images.length === 0) {
-    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    return;
-  }
-
-  const cardsMarkup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-    return `
-      <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" data-src="${largeImageURL}" class="gallery__image" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes:</b> ${likes}
-          </p>
-          <p class="info-item">
-            <b>Views:</b> ${views}
-          </p>
-          <p class="info-item">
-            <b>Comments:</b> ${comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads:</b> ${downloads}
-          </p>
-        </div>
-      </div>
-    `;
-  }).join('');
-
-  gallery.insertAdjacentHTML('beforeend', cardsMarkup);
-
-  
-  const galleryImages = document.querySelectorAll('.gallery__image');
-  galleryImages.forEach(image => {
-    image.addEventListener('click', () => {
-      new SimpleLightbox(`<img src="${image.dataset.src}">`).show();
-    });
-  });
-}
-
-const form = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-
-form.addEventListener('submit', onFormSubmit);
-
-function onFormSubmit(event) {
-  event.preventDefault();
-
-  const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
-
-  gallery.innerHTML = '';
-
-  fetchImages(searchQuery);
-
-  event.currentTarget.reset();
-}
-
-async function searchImages(searchTerm) {
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    renderImages(data.hits);
-  } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-  }
-}
-
-
-  const searchTerm = searchInput.value.trim();
-
-  if (searchTerm === '') {
-    Notiflix.Notify.warning('Please enter a search query!');
-    return;
-  }
-
-  searchImages(searchTerm);
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
+let isShown = 0;
+const newsApiService = new NewsApiService();
 
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+const options = {
+  rootMargin: '50px',
+  root: null,
+  threshold: 0.3,
+};
+const observer = new IntersectionObserver(onLoadMore, options);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  refs.galleryContainer.innerHTML = '';
+  newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  newsApiService.resetPage();
+
+  if (newsApiService.query === '') {
+    Notify.warning('Please, fill the main field');
+    return;
+  }
+
+  isShown = 0;
+  fetchGallery();
+  onRenderGallery(hits);
+}
+
+function onLoadMore() {
+  newsApiService.incrementPage();
+  fetchGallery();
+}
+
+async function fetchGallery() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+
+  const r = await newsApiService.fetchGallery();
+  const { hits, total } = r;
+  isShown += hits.length;
+
+  if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+
+  onRenderGallery(hits);
+  isShown += hits.length;
+
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  }
+
+  if (isShown >= total) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
+
+function onRenderGallery(elements) {
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+      </p>
+    </div>
+    </div>`;
+      }
+    )
+    .join('');
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
